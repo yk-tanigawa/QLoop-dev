232892381a93eae9928c1870623990c20c5df0c7
Yosuke TANIGAWA <yk.tanigawa@gmail.com>
2015-12-17 17:08:28 +0900

    move codes for boosting to adaboost.h; minor change in usage

--- adaboost.h
+++ adaboost.h
@@ -4,6 +4,152 @@
 #include <math.h>
 #include "calloc_errchk.h"
 
+inline void adaDataOnTheFly(const int h_i,
+			 const int h_j,
+			 const int **kmerFreq,
+			 const int k,
+			 short *x){
+  const unsigned long nkmers = 1 << (2 * k);
+  unsigned long l, m;
+  for(l = 0; l < nkmers; l++){
+    for(m = 0; m < nkmers; m++){
+      x[l * nkmers + m] = (((kmerFreq[h_i][l] * kmerFreq[h_j][m]) > 0) ? 1 : 0);
+    }
+  }   
+  return;
+}
+
+inline short adaDataOnTheFlyAxis(const int h_i,
+			      const int h_j,
+			      const int **kmerFreq,
+			      const int k,
+			      const int axis){
+  const unsigned long nkmers = 1 << (2 * k);
+  return (((kmerFreq[h_i][axis / nkmers] * kmerFreq[h_j][axis % nkmers]) > 0) ? 1 : 0);
+}
+
+
+int adaboostLearn(const int *y,
+		  const int *h_i,
+		  const int *h_j,
+		  const int **kmerFreq,
+		  const int k,
+		  const unsigned long T,
+		  const unsigned long N,
+		  const unsigned long dim,
+		  unsigned long **adaAxis,
+		  int **adaSign,
+		  double **adaBeta){
+  short *selected, *x, xaxis;
+  double *w, *p, *err, wsum, epsilon, min, max;
+  unsigned long t, i, d, argmind, argmaxd;
+
+  *adaAxis = calloc_errchk(T, sizeof(unsigned long), "calloc adaAxis");
+  *adaSign = calloc_errchk(T, sizeof(int), "calloc adaSign");
+  *adaBeta = calloc_errchk(T, sizeof(double), "calloc adaBeta");
+
+  selected = calloc_errchk(dim, sizeof(short), "calloc selected");
+  x = calloc_errchk(dim, sizeof(short), "calloc x");
+  w = calloc_errchk(N, sizeof(double), "calloc w");
+  p = calloc_errchk(N, sizeof(double), "calloc p");
+  err = calloc_errchk(dim, sizeof(double), "calloc count");
+
+  for(i = 0; i < N; i++){
+    w[i] = 1.0 / N;
+  }
+
+  for(t = 0; t < T; t++){
+    /* step 1 : compute normalized weights p[] */
+    {
+      wsum = 0;
+      for(i = 0; i < N; i++){
+	wsum += w[i];
+      }
+      for(i = 0; i < N; i++){
+	p[i] = w[i] / wsum;
+      }
+    }
+
+    /* step 2 : find the most appropriate axis (weak lerner) */
+    {
+      for(d = 0; d < dim; d++){
+	err[d] = 0;
+      }
+      for(i = 0; i < N; i++){
+	/* compute data point x */
+	adaDataOnTheFly(h_i[i], h_j[i], kmerFreq, k, x);
+	for(d = 0; d < dim; d++){	  
+	  if(x[d]!= y[i]){
+	    err[d] += p[i];
+	  }
+	}
+      }
+
+      {
+	d = 0;
+	while(selected[d] != 0){
+	  d++;
+	}
+	max = min = err[d];
+	argmaxd = argmind = d;
+	for(d++; d < dim; d++){
+	  if(selected[d] == 0){
+	    if(err[d] < min){
+	      min = err[d];
+	      argmind = d;
+	    }else if(err[d] > max){
+	      max = err[d];
+	      argmaxd = d;
+	    }
+	  }
+	}
+      }
+
+      {
+	if(max + min > 1.0){
+	  /** 
+	   * min > 1 - max 
+	   *  argmaxd is the best axis
+	   */
+	  selected[argmaxd]++;
+	  (*adaAxis)[t] = argmaxd;
+	  (*adaSign)[t] = 1;
+	  epsilon = 1 - max;
+	}else{
+	  /*  argmind is the best axis */
+	  selected[argmind]++;
+	  (*adaAxis)[t] = argmind;
+	  (*adaSign)[t] = 0;
+	  epsilon = min;       
+	}      
+      }
+    }
+
+    /* step 3: compute new weithgts */
+    {
+      (*adaBeta)[t] = epsilon / (1 - epsilon);
+      for(i = 0; i < N; i++){
+	/* compute data */
+	xaxis = adaDataOnTheFlyAxis(h_i[i], h_j[i], kmerFreq, k, (*adaAxis)[t]);
+	if(((*adaSign)[t] == 0 && xaxis == y[i]) ||
+	   ((*adaSign)[t] == 1 && xaxis != y[i])){
+	  w[i] *= (*adaBeta)[t];
+	}
+      }
+    }
+    
+  }
+
+  free(selected);
+  free(x);
+  free(w);
+  free(p);
+  free(err);
+  return 0;
+}
+
+
+#if 0
 int adaboost_apply(const unsigned long *lernerAxis,
 		   const int *lernerPred,
 		   const double *beta,
@@ -23,7 +169,7 @@ int adaboost_apply(const unsigned long *lernerAxis,
   for(i = 0; i < N; i++){
     sum = 0;
     for(t = 0; t < T; t++){
-      if(lernerPred[t] != get_bit(x[i], lernerAxis[t])){
+      if(lernerPred[t] != x[i][lernerAxis[t]]){
 	sum -= log(beta[t]);
       }
     }
@@ -31,5 +177,6 @@ int adaboost_apply(const unsigned long *lernerAxis,
   }
   return 0;
 }
+#endif
 
 #endif
--- kmerPairBoost.c
+++ kmerPairBoost.c
@@ -11,143 +11,10 @@
 
 #include "calloc_errchk.h"
 #include "io.h"
+#include "adaboost.h"
 
 #define F_NAME_LEN 128
 #define BUF_SIZE 4096
-
-inline void adaDataOnTheFly(const int h_i,
-			 const int h_j,
-			 const int **kmerFreq,
-			 const int k,
-			 short *x){
-  const unsigned long nkmers = 1 << (2 * k);
-  unsigned long l, m;
-  for(l = 0; l < nkmers; l++){
-    for(m = 0; m < nkmers; m++){
-      x[l * nkmers + m] = (((kmerFreq[h_i][l] * kmerFreq[h_j][m]) > 0) ? 1 : 0);
-    }
-  }   
-  return;
-}
-
-inline short adaDataOnTheFlyAxis(const int h_i,
-			      const int h_j,
-			      const int **kmerFreq,
-			      const int k,
-			      const int axis){
-  const unsigned long nkmers = 1 << (2 * k);
-  return (((kmerFreq[h_i][axis / nkmers] * kmerFreq[h_j][axis % nkmers]) > 0) ? 1 : 0);
-}
-
-
-int adaboostLearn(const int *y,
-		  const int *h_i,
-		  const int *h_j,
-		  const int **kmerFreq,
-		  const int k,
-		  const unsigned long T,
-		  const unsigned long N,
-		  const unsigned long dim,
-		  unsigned long **adaAxis,
-		  int **adaSign,
-		  double **adaBeta){
-  short *x, xaxis;
-  double *w, *p, *err, wsum, epsilon, min, max;
-  unsigned long t, i, d, argmind, argmaxd;
-
-  *adaAxis = calloc_errchk(T, sizeof(unsigned long), "calloc adaAxis");
-  *adaSign = calloc_errchk(T, sizeof(int), "calloc adaSign");
-  *adaBeta = calloc_errchk(T, sizeof(double), "calloc adaBeta");
-
-  x = calloc_errchk(dim, sizeof(short), "calloc x");
-  w = calloc_errchk(N, sizeof(double), "calloc w");
-  p = calloc_errchk(N, sizeof(double), "calloc p");
-  err = calloc_errchk(dim, sizeof(double), "calloc count");
-
-  for(i = 0; i < N; i++){
-    w[i] = 1.0 / N;
-  }
-
-  for(t = 0; t < T; t++){
-    /* step 1 : compute normalized weights p[] */
-    {
-      wsum = 0;
-      for(i = 0; i < N; i++){
-	wsum += w[i];
-      }
-      for(i = 0; i < N; i++){
-	p[i] = w[i] / wsum;
-      }
-    }
-
-    /* step 2 : find the most appropriate axis (weak lerner) */
-    {
-      for(d = 0; d < dim; d++){
-	err[d] = 0;
-      }
-      for(i = 0; i < N; i++){
-	/* compute data point x */
-	adaDataOnTheFly(h_i[i], h_j[i], kmerFreq, k, x);
-	for(d = 0; d < dim; d++){
-	  if(x[d]!= y[i]){
-	    err[d] += p[i];
-	  }
-	}
-      }
-
-      {
-	max = min = err[0];
-	argmaxd = argmind = 0;
-	for(d = 1; d < dim; d++){
-	  if(err[d] < min){
-	    min = err[d];
-	    argmind = d;
-	  }else if(err[d] > max){
-	    max = err[d];
-	    argmaxd = d;
-	  }
-	}
-      }
-
-      {
-	if(max + min > 1.0){
-	  /** 
-	   * min > 1 - max 
-	   *  argmaxd is the best axis
-	   */
-	  (*adaAxis)[t] = argmaxd;
-	  (*adaSign)[t] = 1;
-	  epsilon = 1 - max;
-	}else{
-	  /*  argmind is the best axis */
-	  (*adaAxis)[t] = argmind;
-	  (*adaSign)[t] = 0;
-	  epsilon = min;       
-	}      
-      }
-    }
-
-    /* step 3: compute new weithgts */
-    {
-      (*adaBeta)[t] = epsilon / (1 - epsilon);
-      for(i = 0; i < N; i++){
-	/* compute data */
-	xaxis = adaDataOnTheFlyAxis(h_i[i], h_j[i], kmerFreq, k, (*adaAxis)[t]);
-	if(((*adaSign)[t] == 0 && xaxis == y[i]) ||
-	   ((*adaSign)[t] == 1 && xaxis != y[i])){
-	  w[i] *= (*adaBeta)[t];
-	}
-      }
-    }
-    
-  }
-
-  free(x);
-  free(w);
-  free(p);
-  free(err);
-  return 0;
-}
 
 int binarization(const double *source,
 		 const double threshold,
@@ -323,14 +190,15 @@ int main_sub(const char *freqFile,
 
 int show_usage(const char *progName){
   printf("usage:\n\n");
-  printf("example: %s -f %s -H %s -k %d -r %d, -t %f -o %s\n", 
+  printf("example: %s -f %s -H %s -k %d -r %d, -t %f -T %ld -o %s\n", 
 	 progName,
-	 "../data/hoge.dat",
-	 "../data/hic.dat",
+	 "./data/hoge.dat",
+	 "./data/hic.dat",
 	 5,
 	 1000,
 	 10.0,
-	 "./");
+	 10L,
+	 "./data/");
   return 0;
 }
 
@@ -345,55 +213,55 @@ int check_params(const char *freqFile,
   int errflag = 0;
 
   if(freqFile == NULL){
-    fprintf(stderr, "input frequency file is not specified\n");
+    fprintf(stderr, "[ERROR] input frequency file is not specified\n");
     errflag++;
-  }else{
-    printf("fasta file:    %s\n", freqFile);
+  }else if(errflag == 0){
+    printf("  k-mer frequency file:    %s\n", freqFile);
   }
 
   if(hicFile == NULL){
-    fprintf(stderr, "Hi-C data directory is not specified\n");
+    fprintf(stderr, "[ERROR] Hi-C data directory is not specified\n");
     errflag++;
-  }else{
-    printf("Hi-C data dir: %s\n", hicFile);
+  }else if(errflag == 0){
+    printf("  Hi-C data dir: %s\n", hicFile);
   }
 
   if(k <= 0){
-    fprintf(stderr, "k is not specified\n");
+    fprintf(stderr, "[ERROR] k is not specified\n");
     errflag++;
-  }else{
-    printf("k: %d\n", k);
+  }else if(errflag == 0){
+    printf("  k: %d\n", k);
   }
 
   if(res <= 0){
-    fprintf(stderr, "resolution is not specified\n");
+    fprintf(stderr, "[ERROR] resolution is not specified\n");
     errflag++;
-  }else{
-    printf("resolution: %d\n", res);
+  }else if(errflag == 0){
+    printf("  resolution: %d\n", res);
   }
 
   if(threshold <= 0){
-    fprintf(stderr, "threshold is not specified\n");
+    fprintf(stderr, "[ERROR] threshold is not specified\n");
     errflag++;
-  }else{
-    printf("threshold: %e\n", threshold);
+  }else if(errflag == 0){
+    printf("  threshold: %e\n", threshold);
   }
 
   if(T <= 0){
-    fprintf(stderr, "number of weak lerners is not specified\n");
+    fprintf(stderr, "[ERROR] number of weak lerners is not specified\n");
     errflag++;
-  }else if(1 << (4 * k) < T){
-    fprintf(stderr, "number of weak lerners(T = %ld) exceeds 16^k (%d)\n", T, 1 << (4 * k));
+  }else if((unsigned long)(1 << (4 * k)) < T){
+    fprintf(stderr, "[ERROR] number of weak lerners(T = %ld) exceeds 16^k (%d)\n", T, 1 << (4 * k));
     errflag++;
-  }else{
-    printf("num. of weak lerners: %ld\n", T);
+  }else if(errflag == 0){
+    printf("  num. of weak lerners: %ld\n", T);
   }
 
   if(outDir == NULL){
-    fprintf(stderr, "output directory is not specified\n");
+    fprintf(stderr, "[ERROR] output directory is not specified\n");
     errflag++;
-  }else{
-    printf("Output dir:    %s\n", outDir);
+  }else if(errflag == 0){
+    printf("  Output dir:    %s\n", outDir);
   }
 
   if(errflag > 0){
