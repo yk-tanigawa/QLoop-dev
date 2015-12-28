846262b80f778318dbc7cec9513253ae66c6f969
Yosuke TANIGAWA <yk.tanigawa@gmail.com>
2015-12-28 22:08:34 +0900

    ctcf_match.py

---
+++ ctcf_match.py
@@ -0,0 +1,49 @@
+import sys;
+
+def enumerate_substr(sequence, k):
+    substr = set([]);
+    for i in range(0, len(sequence) - k + 1):
+        substr.add(sequence[i: i + k]);
+    return substr;
+
+def replace_N(str_set):
+    expanded_set = set([]);
+    for x in str_set:
+        npos = x.find('N');
+        if(npos < 0):
+            expanded_set.add(x);
+        else:
+            rec_set = set([]);
+            for n in ['A', 'C', 'G', 'T']:
+                rec_set.add(x[:npos] + n + x[npos + 1:]);
+            expanded_set.update(replace_N(rec_set));
+    return expanded_set;
+
+def prep_seed_set(targets, k):
+    substr = set([]);
+    for target in targets:
+        if(len(target) >= k):
+            substr.update(enumerate_substr(target, k));
+    return replace_N(substr);
+
+def get_stdin():
+    while True:
+        try:
+            yield ''.join(raw_input());
+        except EOFError:
+            break;
+
+def seed_match(seedset):
+    for line in get_stdin():
+        kmerpair = line.split('\t');
+        seedmatch = [str(kmer in seedset) for kmer in kmerpair[5:9]]
+        print '\t'.join(kmerpair + seedmatch);
+        
+
+def main(argv):
+    targets = ["CCGCGNGGNGGCAG"];
+    seed_set = prep_seed_set(targets, 4);
+    seed_match(seed_set);
+
+if __name__ == "__main__":
+    main(sys.argv);
